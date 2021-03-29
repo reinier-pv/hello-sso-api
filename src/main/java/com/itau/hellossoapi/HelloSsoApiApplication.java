d04f9fb6837ff34f1a303af8a6478c2f1529fca0
Reinier peralta Valdés <reinier-pv@users.noreply.github.com>
2021-03-29 09:44:59 -0300

    first commit

---
+++ src/main/java/com/itau/hellossoapi/HelloSsoApiApplication.java
@@ -0,0 +1,13 @@
+package com.itau.hellossoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSsoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSsoApiApplication.class, args);
+    }
+
+}
